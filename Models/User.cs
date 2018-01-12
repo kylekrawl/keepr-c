fe52b95c0a1d46128f1155f6d2d3df659d67a34f
Kyle Krawl <kyle.krawl@gmail.com>
2018-01-12 10:03:36 -0700

    Add models, user repository, and controller for auth.

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+namespace keepr.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        internal UserReturnModel GetReturnModel()
+        {
+            return new UserReturnModel()
+            {
+                Id = Id,
+                Username = Username,
+                Email = Email
+            };
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,29 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using keepr.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
 
 namespace keepr
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        private readonly string _connectionString = "";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _connectionString = configuration.GetSection("DB").GetValue<string>("mySQLConnectionString");
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+           {
+               options.LoginPath = "/Account/Login/";
+               options.Events.OnRedirectToLogin = (context) =>
+                   {
+                       context.Response.StatusCode = 401;
+                       return Task.CompletedTask;
+                   };
+           });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsDevPolicy", builder =>
+                {
+                    builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                });
+            });
             services.AddMvc();
+            services.AddTransient<IDbConnection>(x => CreateDBContext());
+            services.AddTransient<UserRepository>();
+
+        }
+
+        private IDbConnection CreateDBContext()
+        {
+
+            var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+            return connection;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,8 +69,13 @@ namespace keepr
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsDevPolicy");
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
